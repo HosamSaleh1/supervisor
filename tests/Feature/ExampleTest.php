d1be5f33af0366d4f49a9f54f9ffa5d47f2ef2a2
HosamSaleh1 <mr.hosamsaleh@yahoo.com>
2022-12-14 21:23:31 +0200

    adding unit & feature tests and trying them

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,21 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
+        $response = $this->get('/admin/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_api_login_returns_a_successful_response2()
+    {
+        $response = $this->post('/api/users/login',['email'=>'user@user.com' ,'password'=>'password']);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_api_register_returns_a_successful_response2()
+    {
+        $response = $this->post('/api/users/register',['name'=> fake()->name(),'email'=> fake()->unique()->safeEmail() ,'password'=>'password']);
 
         $response->assertStatus(200);
     }
---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,23 @@
+<?php
+
+// test('example', function () {
+//     expect(true)->toBeTrue();
+// });
+
+namespace Tests\Unit;
+ 
+use PHPUnit\Framework\TestCase;
+ 
+class ProjectTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_basic_test()
+    {
+        $this->assertTrue(true);
+    }
+}
+
